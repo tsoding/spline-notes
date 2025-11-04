9b4781f6cd5eab71bb7fca5f8641c866c1607b0a
rexim <reximkut@gmail.com>
2025-11-04 09:57:25 +0700

    Commit the stream progress

--- nob.c
+++ nob.c
@@ -2,20 +2,60 @@
 #define NOB_STRIP_PREFIX
 #include "nob.h"
 
+Procs procs = {0};
 Cmd cmd = {0};
+
+void cc(void)
+{
+    cmd_append(&cmd, "cc");
+    cmd_append(&cmd, "-Wall");
+    cmd_append(&cmd, "-Wextra");
+    cmd_append(&cmd, "-ggdb");
+}
+
+void raylib_cflags(void)
+{
+    cmd_append(&cmd, "-Iraylib-5.5_linux_amd64/include/");
+}
+
+void raylib_libs(void)
+{
+    cmd_append(&cmd, "-Lraylib-5.5_linux_amd64/lib/");
+    cmd_append(&cmd, "-l:libraylib.a");
+}
+
+void freetype2_cflags(void)
+{
+    cmd_append(&cmd, "-I/usr/include/freetype2");
+}
+
+void freetype2_libs(void)
+{
+    cmd_append(&cmd, "-lfreetype");
+}
 
 int main(int argc, char **argv)
 {
     NOB_GO_REBUILD_URSELF(argc, argv);
-    cmd_append(&cmd, "cc");
-    cmd_append(&cmd, "-Wall");
-    cmd_append(&cmd, "-Wextra");
-    cmd_append(&cmd, "-Iraylib-5.5_linux_amd64/include/");
+    cc();
+    raylib_cflags();
     cmd_append(&cmd, "-o", "spline");
+    cmd_append(&cmd, "spline.c");
+    cmd_append(&cmd, "-lm");
+    raylib_libs();
+    if (!cmd_run(&cmd, .async = &procs)) return 1;
+
+    cc();
+    raylib_cflags();
+    freetype2_cflags();
+    cmd_append(&cmd, "-o", "main");
     cmd_append(&cmd, "main.c");
     cmd_append(&cmd, "-lm");
-    cmd_append(&cmd, "-Lraylib-5.5_linux_amd64/lib/");
-    cmd_append(&cmd, "-l:libraylib.a");
-    if (!cmd_run(&cmd)) return 1;
+    raylib_libs();
+    freetype2_libs();
+    if (!cmd_run(&cmd, .async = &procs)) return 1;
+
+    if (!procs_flush(&procs)) return 1;
+
     return 0;
 }
--- spline.c
+++ spline.c
@@ -1,7 +1,8 @@
-#include <ft2build.h>
-#include FT_FREETYPE_H
+#include <stdio.h>
+#include <stdbool.h>
 #include <raylib.h>
 #include <raymath.h>
+#define NOB_IMPLEMENTATION
 #define NOB_STRIP_PREFIX
 #include "nob.h"
 
@@ -41,6 +42,7 @@ typedef struct {
 } Spline;
 
 static Spline spline = {0};
+static int dragging = -1;
 
 typedef struct {
     float tx;
@@ -136,91 +138,56 @@ void render_spline_into_grid(void)
     }
 }
 
-int main()
+void edit_control_points(void)
 {
-    FT_Library library = {0};
+    Vector2 mouse = GetMousePosition();
 
-    FT_Error error = FT_Init_FreeType(&library);
-    if (error) {
-        fprintf(stderr, "ERROR: Could not initialize FreeType2 library\n");
-        return 1;
+    for (size_t i = 0; i < spline.count; ++i) {
+        Vector2 size = {20, 20};
+        Vector2 position = spline.items[i];
+        position = Vector2Subtract(position, Vector2Scale(size, 0.5));
+
+        bool hover = CheckCollisionPointRec(mouse, (Rectangle) {position.x, position.y, size.x, size.y});
+
+        if (hover) {
+            if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) dragging = i;
+        } else {
+            if (IsMouseButtonReleased(MOUSE_LEFT_BUTTON)) dragging = -1;
+        }
+        DrawRectangleV(position, size, hover ? RED : BLUE);
     }
 
-    const char *const font_file_path = "./Vollkorn-Regular.ttf";
-
-    FT_Face face;
-    error = FT_New_Face(library, font_file_path, 0, &face);
-    if (error == FT_Err_Unknown_File_Format) {
-        fprintf(stderr, "ERROR: `%s` has an unknown format\n", font_file_path);
-        return 1;
-    } else if (error) {
-        fprintf(stderr, "ERROR: Could not load file `%s`\n", font_file_path);
-        return 1;
-    }
-
-    error = FT_Load_Char(face, 'x', FT_LOAD_DEFAULT);
-    // error = FT_Load_Glyph(face, 'Q', FT_LOAD_DEFAULT);
-    if (error) {
-        fprintf(stderr, "ERROR: could not load glyph\n");
-        return 1;
-    }
-
-    long int min_x = LONG_MAX, max_x = LONG_MIN;
-    long int min_y = LONG_MAX, max_y = LONG_MIN;
-    for (int i = 0; i < face->glyph->outline.n_points; ++i) {
-        FT_Vector p = face->glyph->outline.points[i];
-        unsigned char t = face->glyph->outline.tags[i];
-        if (min_x > p.x) min_x = p.x;
-        if (max_x < p.x) max_x = p.x;
-        if (min_y > p.y) min_y = p.y;
-        if (max_y < p.y) max_y = p.y;
-        printf("%ld %ld %02X\n", p.x, p.y, t);
-    }
-
-    printf("x = %ld .. %ld\n", min_x, max_x);
-    printf("y = %ld .. %ld\n", min_y, max_y);
-
-    int factor = 100;
-    int pindex = 0;
-    assert(face->glyph->outline.n_contours == 1);
-    for (int i = 0; i < face->glyph->outline.n_contours; ++i) {
-        for (; pindex <= face->glyph->outline.contours[i]; pindex++) {
-            FT_Vector p = face->glyph->outline.points[pindex];
-            unsigned char t = face->glyph->outline.tags[pindex];
-            float scale = 0.5;
-            float x = (p.x - min_x)*scale + 100;
-            float y = (max_y - p.y)*scale + 100;
-            Vector2 cp = {x, y};
-            da_append(&spline, cp);
-            // DrawCircle(x, y, 8, colors[i%ARRAY_LEN(colors)]);
-            // DrawCircle(x, y, 8, t ? GREEN : RED);
+    if (dragging >= 0) {
+        if (spline.items[dragging].x != mouse.x || spline.items[dragging].y != mouse.y) {
+            render_spline_into_grid();
+        }
+        spline.items[dragging] = mouse;
+    } else {
+        if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
+            da_append(&spline, mouse);
         }
     }
+}
 
-    render_spline_into_grid();
-
-    InitWindow(16*factor, 9*factor, "main");
+int main()
+{
+    InitWindow(window_width, window_height, "Splines");
     SetTargetFPS(60);
     while (!WindowShouldClose()) {
-        ClearBackground(BLACK);
         BeginDrawing();
+        ClearBackground(GetColor(0x181818FF));
+        if (IsKeyPressed(KEY_F2)) {
+            spline.count = 0;
+            for (size_t y = 0; y < grid_height; ++y) {
+                for (size_t x = 0; x < grid_width; ++x) {
+                    grid[y][x] = false;
+                }
+            }
+        }
+        edit_control_points();
         display_grid();
         EndDrawing();
     }
     CloseWindow();
-
-    // Types we are interested in
-    // - FT_Outline from ftimage.h
-    // - FT_Vector
-    //
-    // - FT_OutlineGlyphRec
-    // - FT_GlyphSlotRec
-    // *   You can typecast an @FT_Glyph to @FT_OutlineGlyph if you have
-    // *   `glyph->format == FT_GLYPH_FORMAT_OUTLINE`.  This lets you access the
-    // *   outline's content easily.
-    // - FT_Glyph
-
-
-    printf("OK\n");
     return 0;
 }
